ba8f4ead73bf5a05d9ff1d632f604a7bd3089528
Muhammed Berisso <muhammedb8826@gmail.com>
2023-06-17 18:44:35 +0300

    Add a test for custom methods inside post model

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -28,4 +28,25 @@ RSpec.describe Post, type: :model do
       expect(subject).not_to be_valid
     end
   end
+
+  context "update_post_counter" do
+    let!(:author) { User.create name: 'Dr.Zakir', posts_counter: 0 }
+    let!(:post) { Post.create author_id: author.id, title: 'Hajj', comments_counter: 0, likes_counter: 0 }
+
+    it 'should increment posts_counter by one' do
+      expect(post.update_post_counter.posts_counter).to eq(2)
+    end
+  end
+
+  context "recent_comments" do
+    let!(:author) { User.create name: 'Dr.Zakir', posts_counter: 0 }
+    let!(:post) { Post.create author_id: author.id, title: 'Hajj', comments_counter: 0, likes_counter: 0 }
+    let!(:comment) do
+      Comment.create author_id: author.id, post_id: post.id, text: 'Prayer is the most important thing in Islam'
+    end
+
+    it 'should return recent comments' do
+      expect(post.recent_comments).to eq([comment])
+    end
+  end
 end
