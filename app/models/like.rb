f7bbe81292687083e21e340d7a172be2ed54b4b8
Muhammed Berisso <muhammedb8826@gmail.com>
2023-06-17 16:44:18 +0300

    Generate spec and create a test for models

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,9 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_like_counter
+  after_save :update_likes_counter
 
-  def update_like_counter
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
