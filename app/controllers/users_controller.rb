68a34ba45d90d2eb07db795ee54e3d318c50037b
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-06-24 00:52:38 +0300

    Test controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,30 +3,7 @@ class UsersController < ApplicationController
     @users = User.all # assuming you have a User model
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(params[:user])
-    # rest of your create method as usual
-  end
-
   def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    # rest of your update method as usual
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    # rest of your destroy method as usual
+    @user = User.find_by_id(params[:id])
   end
 end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts" do
+    it "works! (now write some real specs)" do
+      get '/users/1/posts'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include("This is from post page")
+    end
+  end
+  describe "GET /posts/:id" do
+    it "works! (now write some real specs)" do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Post 1")
+    end
+  end
+end
