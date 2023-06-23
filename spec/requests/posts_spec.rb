cce7f75c2f8fb50e7206c3696514a42ca67a9ae4
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-06-24 00:59:57 +0300

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /posts' do
+    it 'works! (now write some real specs)' do
       get '/users/1/posts'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
-      expect(response.body).to include("This is from post page")
+      expect(response.body).to include('This is from post page')
     end
   end
-  describe "GET /posts/:id" do
-    it "works! (now write some real specs)" do
+  describe 'GET /posts/:id' do
+    it 'works! (now write some real specs)' do
       get '/users/1/posts/1'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
-      expect(response.body).to include("Post 1")
+      expect(response.body).to include('Post 1')
     end
   end
 end
