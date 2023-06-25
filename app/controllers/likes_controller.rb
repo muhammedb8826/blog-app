561634a4b494cbd6522780f9fb6dfa75b2af249b
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-06-25 07:36:05 +0300

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,16 @@
 class LikesController < ApplicationController
-    def new
-      @like = Like.new
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.new(author_id: current_user.id, post_id: params[:post_id])
+
+    if @like.save
+      flash[:notice] = 'Your like was saved successfully'
+    else
+      flash[:alert] = 'Something went wrong, please try again.'
     end
-  
-    def create
-      @like = Like.new(author_id: current_user.id, post_id: params[:post_id])
-  
-      if @like.save
-        flash[:notice] = 'Your like was saved successfully'
-      else
-        flash[:alert] = 'Something went wrong, please try again.'
-      end
-      redirect_to user_posts_path(params[:user_id])
-    end
+    redirect_to user_posts_path(params[:user_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
   def new
     @post = Post.new
   end
