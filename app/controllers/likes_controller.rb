0d30fb6760b4d24b7f0be06e9d66439b15fbfe38
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-02 02:12:38 +0300

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,4 @@
 class LikesController < ApplicationController
- 
   before_action :authenticate_user!
 
   def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   before_action :authenticate_user!
 
   before_action :set_user
