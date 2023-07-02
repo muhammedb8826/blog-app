fde2aa8dcb411d0c30013e9c0b5d6eb6a403d10e
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-02 04:53:00 +0300

    Add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
-  
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   resources :users, only: [:index, :show] do
     resources :posts do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
