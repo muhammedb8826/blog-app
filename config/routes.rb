30c473645200b51aad8cbf2fcd743dea4f5e26a7
getssh <gtamrat33@gmail.com>
2023-07-02 05:04:15 +0300

    Add destroy feature to comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index, :show] do
     resources :posts do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
