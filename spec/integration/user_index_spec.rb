a5ca534e731cac96c3675e312bd6b162da9e0940
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-05 23:46:12 +0300

    Add integration test for user index page

---
+++ spec/integration/user_index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+RSpec.describe 'User index integration spec', type: :system do
+  describe 'index page' do
+    let!(:user_two) { User.create name: 'Mufti menk', posts_counter: 0 }
+    let!(:user_three) { User.create name: 'Dr Zakir', posts_counter: 0 }
+    it 'should render the user name' do
+      visit users_path
+      expect(page).to have_content(user_two.name)
+    end
+    it 'should render the users photo' do
+      visit users_path
+      expect(page).to have_content(user_two.photo)
+    end
+    it 'should render the number of posts' do
+      visit users_path
+      expect(page).to have_content("Number of posts #{user_two.posts.size}")
+    end
+    # When I click on a user, I am redirected to that user's show page.
+    it 'should redirected to that users show page' do
+      visit users_path
+      click_on user_two.name
+      expect(page).to have_content(user_two.name)
+    end
+  end
+end
