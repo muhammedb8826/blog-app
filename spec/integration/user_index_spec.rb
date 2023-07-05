3ef84f41257a03ae4fda36c6ab93dc44d5af0597
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-06 02:44:07 +0300

    Add user index requested tests

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -21,5 +21,23 @@ RSpec.describe 'User index integration spec', type: :system do
       click_on user_two.name
       expect(page).to have_content(user_two.name)
     end
+    
+    # I can see the username of all other users.
+    it 'should render the username of all other users' do
+        visit users_path
+        expect(page).to have_content(user_three.name)
+    end
+   
+    # I can see the profile picture for each user.
+    it 'should render the profile picture for each user' do
+        visit users_path
+        expect(page).to have_content(user_three.photo)
+    end
+   
+    # I can see the number of posts each user has written.
+    it 'should render the number of posts each user has written' do
+        visit users_path
+        expect(page).to have_content("Number of posts #{user_three.posts.size}")
+    end
   end
 end
