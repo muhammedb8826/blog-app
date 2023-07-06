a5c4317788057469b8843671e8d5c9277a944ac1
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-06 23:47:58 +0300

    Add requested views for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "users#index"
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts do
       resources :comments, only: [:new, :create]
