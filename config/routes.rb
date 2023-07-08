cefff95b38930acb2b42af5b1aa07fc328a6b338
getssh <gtamrat33@gmail.com>
2023-07-08 15:06:55 +0300

    Add paths controller to route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "users#index"
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :users, only: [:index, :show] do
     resources :posts do
       resources :comments, only: [:new, :create, :destroy]
